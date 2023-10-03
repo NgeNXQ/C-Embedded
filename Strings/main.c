b18fbb81a2e3b5d2e2030bb30bb54e28d80db602
Denys Babich <89969565+NgeNXQ@users.noreply.github.com>
2023-10-04 02:50:37 +0300

    Added reverse string

---
+++ Strings/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+char* readString(const char*);
+char* reverseString(const char*);
+
+int main()
+{
+    char* string = readString("Enter a string: ");
+    printf(string);
+
+    //printf(reverseString());
+    return 0;
+}
+
+char* readString(const char* message)
+{
+    char* string = (char*)malloc(101 * sizeof(char));
+
+    if (string == NULL)
+    {
+        perror("Memory allocation failed");
+        return NULL;
+    }
+
+    printf("%s", message);
+    scanf_s("%s", string);
+
+    return string;
+}
+
+char* reverseString(const char* string)
+{
+    int length = strlen(string);
+    char* newString = (char*)malloc((length + 1) * sizeof(char));
+
+    if (newString == NULL) 
+    {
+        perror("Memory allocation failed");
+        return NULL;
+    }
+
+    for (int i = length - 1, j = 0; i >= 0; i--, j++) 
+        newString[j] = string[i];
+
+    newString[length] = '\0';
+
+    return newString;
+}
