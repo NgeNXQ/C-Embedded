2596466f2bd98d96ae67c79700da186800648043
Denys Babich <89969565+NgeNXQ@users.noreply.github.com>
2023-10-11 23:28:55 +0300

    Removed random.h and random.c

--- 1/LinkedList/random.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-#include "random.h"
-
-float random_float(const float min, const float max)
-{
-	return ((float)rand() / RAND_MAX) * (max - min + 1) + min;
-}
--- 1/LinkedList/random.h
+++
@@ -1,6 +0,0 @@
-#ifndef RANDOM_H
-#define RANDOM_H
-
-float random_float(const float min, const float max);
-
-#endif // RANDOM_H
